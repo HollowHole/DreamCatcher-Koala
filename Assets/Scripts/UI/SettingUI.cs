80eab54170a9b426508c7579309230d48057f970
HollowHole <166734293+HollowHole@users.noreply.github.com>
2024-11-27 00:11:18 +0800

    游戏暂停功能（BUG）
    过度动画会取消暂停游戏，暂停游戏会因为动画结束导致Timescale置1

--- Assets/Scripts/UI/SettingUI.cs
+++ Assets/Scripts/UI/SettingUI.cs
@@ -1,12 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class SettingUI : MonoBehaviour
 {
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void Initialize()
     {
-        //Instantiate();
+        GameObject myPrefab = Resources.Load("Settings") as GameObject;
+        GameObject instance = Instantiate(myPrefab);
+        DontDestroyOnLoad(instance);
     }
+
+    [SerializeField]GameObject panel;
+
+    private void Start()
+    {
+        ResumeGame();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (panel.activeInHierarchy)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void ResumeGame()
+    {
+        panel.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void PauseGame()
+    {
+        panel.SetActive (true);
+        Time.timeScale = 0;
+    }
+
 }
