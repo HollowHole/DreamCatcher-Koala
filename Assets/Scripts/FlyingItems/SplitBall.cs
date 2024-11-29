af9bbfd68578bd379c8f509c88897d84fc275ada
LuAliSora <166391258+LuAliSora@users.noreply.github.com>
2024-11-29 12:20:20 +0800

    hitwall（unfinish）

---
+++ Assets/Scripts/FlyingItems/SplitBall.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SplitBall : Ball
+{
+    [SerializeField] Sprite HostileImage;
+    [SerializeField] Sprite FriendlyImage;
+    [SerializeField] RuntimeAnimatorController HostileAnimCrl;
+    [SerializeField] RuntimeAnimatorController FriendlyAnimCrl;
+    public override void UpdateView()
+    {
+        if (isHostile)
+        {
+            mImg.sprite = HostileImage;
+            animator.runtimeAnimatorController = HostileAnimCrl;
+        }
+        else
+        {
+            mImg.sprite = FriendlyImage;
+            animator.runtimeAnimatorController = FriendlyAnimCrl;
+        }
+    }
+}
