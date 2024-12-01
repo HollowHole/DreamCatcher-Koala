22308a1ab09c84d0f825e5a6a1b2923e4755f105
HollowHole <166734293+HollowHole@users.noreply.github.com>
2024-12-01 17:39:01 +0800

    转场

--- Assets/Scripts/StartingManager.cs
+++ Assets/Scripts/StartingManager.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -6,27 +7,47 @@ using UnityEngine;
 public class StartingManager : MonoBehaviour
 {
     [SerializeField] GameObject InputTip;
+    [SerializeField] GameObject Koala1;
+    [SerializeField] GameObject Koala2;
+    [SerializeField] Transform transitionCam;
+    [SerializeField] float transitionTime = 2f;
+    [SerializeField] GameObject ExitButton;
     private IEnumerator Start()
     {
         InputTip.SetActive(false);
-
+        transitionCam.gameObject.SetActive(false);
+        Koala1.SetActive(true);
+        Koala2.SetActive(false);
         while (true)
         {
-            if (!Input.GetMouseButtonDown(0))
+            if (Input.GetMouseButtonDown(0))
                 break;
             yield return null;
         }
 
         //开始游戏
-
-        yield return new WaitForSeconds(3f);
+        Koala1.SetActive(false);
+        Koala2.SetActive(true);
+        yield return new WaitForSeconds(7.833f);
 
         InputTip.SetActive(true);
+        ExitButton.SetActive(false);
 
     }
     public void CloseInputTipPanel()
     {
-        LevelManager.Instance.LoadNextLevelAsync();
+        StartCoroutine(LoadLevel1());
+    }
+    IEnumerator LoadLevel1()
+    {
+        
         InputTip.SetActive(false);
+        transitionCam.gameObject.SetActive(true);
+        transitionCam.GetComponent<Camera>().orthographicSize = 0.1f;
+        transitionCam.GetComponent<Camera>().DOOrthoSize(Camera.main.orthographicSize, transitionTime).From();
+        transitionCam.DOMove(Camera.main.transform.position, transitionTime).From();
+
+        yield return new WaitForSeconds(transitionTime);
+        LevelManager.Instance.LoadNextLevelAsync();
     }
 }
--- Assets/Scripts/TransitionAnim.cs
+++ Assets/Scripts/TransitionAnim.cs
@@ -4,16 +4,20 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
+using static System.TimeZoneInfo;
 
 public class TransitionAnim : MonoBehaviour
 {
     public static float TransitionLastTime = 2f;
+    [SerializeField] Camera transitionCam;
+    [SerializeField] Camera mainCam;
     // Start is called before the first frame update
     Image image;
     
     private void Awake()
     {
         image = GetComponent<Image>();
+        transitionCam = transform.GetChild(0).GetComponent<Camera>();
     }
     private void Start()
     {
@@ -21,46 +25,74 @@ public class TransitionAnim : MonoBehaviour
     }
     public void PlayExitAnimAndSwitchScene()//Will try unload scene in this function
     {
-        image.fillAmount = 0;
-        Tweener tweener = image.DOFillAmount(1, TransitionLastTime);
+        //image.fillAmount = 0;
+        //Tweener tweener = image.DOFillAmount(1, TransitionLastTime);
+        Tweener tweener;
+        transitionCam.gameObject.SetActive(true);
+        transitionCam.orthographicSize = 0.1f;
+
+        transitionCam.transform.position = PlayerController.Instance.transform.position;
+        transitionCam.transform.position += new Vector3(0, 0, -5);
+        tweener = transitionCam.DOOrthoSize(mainCam.orthographicSize, TransitionLastTime).From();
+        tweener.SetUpdate(true);
+        tweener =  transitionCam.transform.DOMove(mainCam.transform.position, TransitionLastTime).From();
+
         tweener.SetUpdate(true);
         tweener.SetAutoKill(false);
         StartCoroutine(WaitAnimEndSwitchScene(tweener));
     }
     void PlayStartSceneAnim()//Then Start Game in this function
     {
-        image.fillAmount = 1;
-        Tweener tweener = image.DOFillAmount(0, TransitionLastTime);
+        //image.fillAmount = 1;
+        //Tweener tweener = image.DOFillAmount(0, TransitionLastTime);
+        Tweener tweener;
+        transitionCam.gameObject.SetActive(true);
+        transitionCam.transform.position = PlayerController.Instance.transform.position;
+        transitionCam.transform.position += new Vector3(0, 0, -5);
+        tweener = transitionCam.DOOrthoSize(mainCam.orthographicSize, TransitionLastTime);
         tweener.SetUpdate(true);
-        StartCoroutine(WaitAnimEndPlayerStartGame());
+        tweener = transitionCam.transform.DOMove(mainCam.transform.position, TransitionLastTime);
+
+        tweener.SetUpdate(true);
+        tweener.SetAutoKill(false);
+        StartCoroutine(WaitAnimEndPlayerStartGame(tweener));
     }
 
     private void OnDestroy()
     {
         //Debug.Log("listener disposed");
     }
-    IEnumerator WaitAnimEndPlayerStartGame()
+    IEnumerator WaitAnimEndPlayerStartGame(Tweener tweener)
     {
         //float playerFallingSpeed = PlayerController.Instance.FallingSpeed;
         //PlayerController.Instance.FallingSpeed = 0;
         Time.timeScale = 0;
 
-        yield return new WaitForSecondsRealtime(TransitionAnim.TransitionLastTime);
+        while (!tweener.IsComplete())
+        {
+            yield return null;
+        }
 
         Debug.Log("Start Game");
 
         if(!SettingUI.isGamePause)
             Time.timeScale = 1;
 
+        transitionCam .gameObject.SetActive(false);
         //PlayerController.Instance.FallingSpeed = playerFallingSpeed;
     }
     IEnumerator WaitAnimEndSwitchScene(Tweener tweener)
     {
+        Time.timeScale = 0;
+
         while (!tweener.IsComplete())
         {
             yield return null;
         }
-
+        if (!SettingUI.isGamePause)
+            Time.timeScale = 1;
         LevelManager.Instance.LoadNextLevelAsync();
+
+        transitionCam.gameObject.SetActive(false);
     }
 }
