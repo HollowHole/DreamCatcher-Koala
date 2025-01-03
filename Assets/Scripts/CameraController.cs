f9cbdeb82ea6e124f0193f863c1dde38808fb407
LuAliSora <166391258+LuAliSora@users.noreply.github.com>
2025-01-03 22:06:19 +0800

    temp

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -12,6 +12,19 @@ public class CameraController : MonoBehaviour
 
     static private float scP=0f;
 
+    [SerializeField]
+    private float scrollSpeed;
+    [SerializeField]
+    private float minAngle; // 相机俯仰角的最小值
+    [SerializeField]
+    private float maxAngle; // 相机俯仰角的最大值
+    [SerializeField]
+    private float orbitRadius;
+    [SerializeField]
+    private float lastAngle;
+    [SerializeField]
+    private float oriZ;
+
     public float score{
         get{
             // Debug.Log("Get_ballNum: "+ballN);
@@ -23,6 +36,13 @@ public class CameraController : MonoBehaviour
         }
     }
 
+    void Awake(){
+        scrollSpeed = 100f;
+        minAngle = -30f;
+        maxAngle = 30f;
+        lastAngle=0f;
+        oriZ=-10f;
+    }
     private void Start()
     {
         isFollowingPlayer = false;
@@ -37,7 +57,13 @@ public class CameraController : MonoBehaviour
         {
             Vector3 pos = transform.position;
             pos.y = player.position.y + PlayerOffset;
+            pos.z=oriZ;
             transform.position = pos;
+
+            Vector3 rCenter=player.position+new Vector3(0f,PlayerOffset,0f);
+
+            transform.rotation=Quaternion.identity;
+            RollCamera(rCenter);
 
             score+=Time.deltaTime;
         }
@@ -52,7 +78,31 @@ public class CameraController : MonoBehaviour
         isFollowingPlayer=flag;
         if(flag)
         {
-            PlayerOffset = (transform.position - player.position).y;
+            Vector3 temp=(transform.position - player.position);
+            PlayerOffset = temp.y;
+            orbitRadius=temp.magnitude;
+        }
+    }
+
+    void RollCamera(Vector3 rCenter){
+        Vector3 axisTemp=new Vector3(1,0,0);
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (scroll != 0.0f)
+        {
+
+            // transform.position = playerPos + transform.forward * orbitRadius;
+            // transform.LookAt(playerPos); // 确保相机面向原点
+            // 计算相机应该旋转的角度
+            float angleNew = scroll * scrollSpeed;
+            float angleChange = Mathf.Clamp(lastAngle+angleNew, minAngle, maxAngle);
+            Debug.Log(angleChange);
+
+            transform.RotateAround(rCenter, axisTemp, angleChange);
+            lastAngle=angleChange;
+        }
+        else{
+            transform.RotateAround(rCenter, axisTemp, lastAngle);
         }
     }
 }
